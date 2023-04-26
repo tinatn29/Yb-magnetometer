646ebc153acff326a81a3af2b803010bbcef40a4
tinatn29 <tanaporn.nanarong@gmail.com>
2023-04-26 15:28:19 -0700

    job submission file for linear bz

--- submission_files/submit_bz_basic.py
+++ submission_files/submit_bz_basic.py
@@ -8,20 +8,20 @@ import numpy as np
 
 # single job submission (1000 atoms)
 delta_mod = float(sys.argv[1]) # [1] <delta_mod / gamma> e.g. 20 means delta_mod = 20 * gamma
-theta = float(sys.argv[2]) # [2] <theta / pi> e.g. 0.5 means pi/2
+theta = float(sys.argv[2]) # [2] <theta / pi> e.g. 90 means 90 degrees
 light_fields = sys.argv[3] # [3] light fields as strings e.g. "[(10,-1),(10,1)]"
 b_array_fname = "bz_linear_75.npy" # basic bz linear gradient from 0 to 75
 
 light_fields_list = ast.literal_eval(light_fields) # list format of light_fields
 num_freqs = len(light_fields_list)
 
-job_file = os.path.join(os.getcwd(),f"./jobs/{delta_mod:.0f}_fixed1000_{theta:.2f}pi_{num_freqs}bands_bz75.sh")
+job_file = os.path.join(os.getcwd(),f"./jobs/{delta_mod:.0f}_fixed1000_{theta:.0f}deg_{num_freqs}bands_bz75.sh")
 
 with open(job_file, 'w') as fh:
 	fh.writelines("#!/bin/bash\n")
-	fh.writelines(f"#SBATCH --job-name={delta_mod:.0f}_fixed1000_{theta:.2f}pi_{num_freqs}bands_bz75.job\n")
-	fh.writelines(f"#SBATCH --output=./out/{delta_mod:.0f}_fixed1000_{theta:.2f}pi_{num_freqs}bands_bz75.txt\n")
-	fh.writelines(f"#SBATCH --error=./err/{delta_mod:.0f}_fixed1000_{theta:.2f}pi_{num_freqs}bands_bz75.txt\n")
+	fh.writelines(f"#SBATCH --job-name={delta_mod:.0f}_fixed1000_{theta:.0f}deg_{num_freqs}bands_bz75.job\n")
+	fh.writelines(f"#SBATCH --output=./out/{delta_mod:.0f}_fixed1000_{theta:.0f}deg_{num_freqs}bands_bz75.txt\n")
+	fh.writelines(f"#SBATCH --error=./err/{delta_mod:.0f}_fixed1000_{theta:.0f}deg_{num_freqs}bands_bz75.txt\n")
 	fh.writelines("#SBATCH -c 32\n")
 	fh.writelines("#SBATCH -N 1\n")
 	fh.writelines("#SBATCH --time=0-6:00\n")
@@ -31,6 +31,6 @@ with open(job_file, 'w') as fh:
 	fh.writelines("ml python/3.6.1\n")
 	fh.writelines([f"python3 $SCRATCH/AT_data/RunMC_configs_fixed1000.py {delta_mod} {theta} ", '"', light_fields, '" ',\
 		b_array_fname, 
-		f" {delta_mod:.0f}_fixed1000_{theta:.2f}pi_{num_freqs}bands_bz75.csv"])
+		f" {delta_mod:.0f}_fixed1000_{theta:.0f}deg_{num_freqs}bands_bz75.csv"])
 
 os.system("sbatch %s" %job_file)
