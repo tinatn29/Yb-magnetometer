7df73259923cad58ae8df939ba74269f64585a32
tinatn29 <tanaporn.nanarong@gmail.com>
2023-05-04 12:23:35 -0700

    add RunMC and job submission files that take bx,by as args

---
+++ RunMC_Josh_bxby_fixed1000.py
@@ -0,0 +1,61 @@
+import numpy as np
+import csv
+import sys
+import ast
+from ATSolver import ATSolver
+
+
+'''
+RunMC_bxby_fixed1000.py 
+This file is for Sherlock submission and takes fixed bx, by values (with weak bz gradient):
+[1] <delta_mod / gamma> e.g. 20 means delta_mod = 20 * gamma
+[2] <theta / deg> e.g. 90 = 90 degrees = pi/2
+[3] <light_fields> as strings e.g. "[(10,-1),(10,1)]" would create 2 sidebands with 
+Omega1=Omega2=10 and detunings = -1*delta_mod and 1*delta_mod from m=0 state
+[4] <bx/gamma> fixed bx value
+[5] <by/gamma> fixed by value
+[6] <filename> e.g. "filename.csv" where the result is saved
+
+'''
+
+# Set simulation parameters
+delta_mod = float(sys.argv[1])  # modulation freq
+theta_pol = float(sys.argv[2]) * np.pi / 180  # convert polarization angle from deg to rad
+light_fields_arg = sys.argv[3]
+light_fields = ast.literal_eval(light_fields_arg) # convert string argument to list of tuples
+# [(Omega1, delta1), (Omega2, delta2), ...]
+# detuning from m=0 level = delta * delta_mod 
+# delta=2 -> 2nd harmonic sideband with detuning = 2*delta_mod from resonance
+
+# Initialize the solver
+AT = ATSolver(light_fields, delta_mod, theta_pol)
+
+# Prepare input B-field array and velocity
+# vx_input = np.load('./configs/vx_input_1000.npy') # load transverse vx from file
+vx_input = np.array([0, 0.01, 0.1, 0.2]) # test input
+
+bz = np.linspace(-15, 15, 121)
+bx_fixed = float(sys.argv[4]) # take bx from argument
+bx = np.ones_like(bz) * bx_fixed
+by_fixed = float(sys.argv[5]) # take by from argument
+by = np.ones_like(bz) * by_fixed 
+
+AT.b_array = np.array([bx, by, bz]).transpose()
+
+# calculate Doppler-averaged results
+results = AT.SolveME_parallel_b_array(vx_input, max_cores=4)
+result = np.mean(results, axis=0) # average down each column
+
+# Filename from argument
+filename = "test_20230504.csv"
+np.savetxt(filename, result, delimiter=',') # save result as csv 
+
+'''
+# calculate Doppler-averaged results
+results = AT.SolveME_parallel_b_array(vx_input)
+result = np.mean(results, axis=0) # average down each column
+
+# Filename from argument
+filename = sys.argv[6]
+np.savetxt(filename, result, delimiter=',') # save result as csv 
+'''
---
+++ submission_files/submit_Josh_bxby_thetas.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import ast
+import numpy as np
+
+
+# single job submission (1000 atoms)
+delta_mod = float(sys.argv[1]) # [1] <delta_mod / gamma> e.g. 20 means delta_mod = 20 * gamma
+light_fields = sys.argv[2] # [2] light fields as strings e.g. "[(10,-1),(10,1)]"
+light_fields_list = ast.literal_eval(light_fields) # list format of light_fields
+num_freqs = len(light_fields_list)
+
+# custom fixed bx by values
+bx = float(sys.argv[3])
+by = float(sys.argv[4])
+
+thetas = np.array([0.0, 11.2, 22.5, 33.8, 45.0, 56.2, 67.5, 78.8, 90.0]) 
+# polarization angles from Josh's data
+
+for theta in thetas:
+	job_file = os.path.join(os.getcwd(),f"./jobs/{delta_mod:.0f}_fixed1000_{theta:.0f}deg_{num_freqs}bands_bx{bx:.1f}_by{by:.1f}.sh")
+
+	with open(job_file, 'w') as fh:
+		fh.writelines("#!/bin/bash\n")
+		fh.writelines(f"#SBATCH --job-name={delta_mod:.0f}_fixed1000_{theta:.0f}deg_{num_freqs}bands_bx{bx:.1f}_by{by:.1f}.job\n")
+		fh.writelines(f"#SBATCH --output=./out/{delta_mod:.0f}_fixed1000_{theta:.0f}deg_{num_freqs}bands_bx{bx:.1f}_by{by:.1f}.txt\n")
+		fh.writelines(f"#SBATCH --error=./err/{delta_mod:.0f}_fixed1000_{theta:.0f}deg_{num_freqs}bands_bx{bx:.1f}_by{by:.1f}.txt\n")
+		fh.writelines("#SBATCH -c 32\n")
+		fh.writelines("#SBATCH -N 1\n")
+		fh.writelines("#SBATCH --time=0-6:00\n")
+		fh.writelines("#SBATCH --mem=64000\n")
+		fh.writelines("#SBATCH --qos=normal\n")
+		fh.writelines("#SBATCH -p hns\n")
+		fh.writelines("ml python/3.6.1\n")
+		fh.writelines([f"python3 $SCRATCH/AT_data/RunMC_Josh_bxby_fixed1000.py {delta_mod} {theta} ", '"', light_fields, '" ',\
+			f"{bx} {by} {delta_mod:.0f}_fixed1000_{theta:.0f}deg_{num_freqs}bands_bx{bx:.1f}_by{by:.1f}.csv"])
+
+	os.system("sbatch %s" %job_file)
